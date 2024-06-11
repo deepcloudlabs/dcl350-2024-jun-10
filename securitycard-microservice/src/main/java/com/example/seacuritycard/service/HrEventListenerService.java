d5ad0f080310b5a50c95d60db5bed8986c17bf58
Binnur KURT <deepcloudlabs@gmail.com>
2024-06-11 14:19:03 +0300

    initial commit

---
+++ securitycard-microservice/src/main/java/com/example/seacuritycard/service/HrEventListenerService.java
@@ -0,0 +1,13 @@
+package com.example.seacuritycard.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HrEventListenerService {
+
+	@KafkaListener(topics = "${hrEventsTopicName}", groupId = "security-card")
+	public void listenHrEvents(String event) {
+		System.err.println("[HrEventListenerService] New hr event has arrived: %s".formatted(event));
+	}
+}
