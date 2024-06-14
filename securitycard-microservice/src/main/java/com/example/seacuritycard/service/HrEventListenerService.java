2eea01f43573e42d7f433833e1d82558d2b11cf3
Binnur KURT <deepcloudlabs@gmail.com>
2024-06-14 12:10:17 +0300

    initial commit

--- securitycard-microservice/src/main/java/com/example/seacuritycard/service/HrEventListenerService.java
+++ securitycard-microservice/src/main/java/com/example/seacuritycard/service/HrEventListenerService.java
@@ -1,5 +1,6 @@
 package com.example.seacuritycard.service;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,12 @@ public class HrEventListenerService {
 		System.err.println("[Kafka][HrEventListenerService] New hr event has arrived: %s".formatted(event));
 	}
 
+
+	@RabbitListener(queues = "hrque")
+	public void listenRandomEvents(String event) {
+		System.err.println("[RabbitMQ][listenRandomEvents] New random event has arrived: %s".formatted(event));
+	}
+	
 	@KafkaListener(topics = "crm-events", groupId = "security-card")
 	public void listenCrmEvents(String event) {
 		System.err.println("[Kafka][CrmEventListenerService] New crm event has arrived: %s".formatted(event));
